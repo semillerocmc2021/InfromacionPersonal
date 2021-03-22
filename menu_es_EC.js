c0cf405cc3071a2155ef2a984172eca1ae2d4f0e
bapilco <bryanpilco.1597@gmail.com>
2021-03-22 15:45:25 -0500

    Menu Alexander Pilco

--- menu_es_EC.js
+++ menu_es_EC.js
@@ -3,7 +3,8 @@
         "MENU": {
             "MNU_FASE_4":"Menú Fase 4",
 			"MNU_LUIS_ANGEL_FLOREZ":"Menú Luis Florez",
-			"MNU_MFFR":"Maria Fernanda Florez"
+			"MNU_MFFR":"Maria Fernanda Florez",
+			"MNU_PLCO_ALEXANDER":"Menu Alexander Pilco"
         }
     }
 }

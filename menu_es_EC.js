e9014cd750d0b36871381716204cabe34fe23e78
jorge <jorgemosquera1150@gmail.com>
2021-03-22 15:58:01 -0500

    Agrege mi etiqueta del menu

--- menu_es_EC.js
+++ menu_es_EC.js
@@ -8,7 +8,8 @@
             "MNU_JEAN_RAVE":"Jean Rave",
             "MNU_JCRR_GRID":"registrar Producto Jean Rave",
             "MNU_JCRR_GRID2":"Lista Productos Jean Rave",
-            "MNU_JCRR_GRID_CLIENTE":"Busqueda Clientes Jean Rave"
+            "MNU_JCRR_GRID_CLIENTE":"Busqueda Clientes Jean Rave",
+            "MNU_JMS":"Menu Jorge Mosquera"
         }
     }
 }

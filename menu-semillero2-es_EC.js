c52b9b7af573572a3e0dfcb40f1c0b5fc5dfc765
Jean Rave <jeanclon200021@gmail.com>
2021-03-22 17:16:03 -0500

    Cambio de la etiqueta Jean Rave a Capacitación Jean Rave

--- menu-semillero2-es_EC.js
+++ menu-semillero2-es_EC.js
@@ -5,7 +5,7 @@
 			"MNU_LUIS_ANGEL_FLOREZ":"Luis Fl&oacute;rez",
 			"MNU_MFFR":"Maria Fernanda Florez",
 			"MNU_PLCO_ALEXANDER":"Menu Alexander Pilco",
-			"MNU_JEAN_RAVE":"Jean Rave",
+			"MNU_JEAN_RAVE":"Capacitaci\u00F3n Jean Rave",
 			"MNU_JCRR_GRID":"registrar Producto Jean Rave",
             "MNU_JCRR_GRID2":"Lista Productos Jean Rave",
             "MNU_JCRR_GRID_CLIENTE":"Busqueda Clientes Jean Rave",

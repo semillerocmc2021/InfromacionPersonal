497ccf87be256cb3dce1e93e88e72230ea286af7
smosquera <smosquera@clearminds-it.com>
2021-03-22 02:10:28 -0500

    menu 5

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -2,8 +2,11 @@
     "COMMONS": {
         "MENU": {
 			"MNU_SMCMC":"Formularios Semillero",
-			"MNU_EJEMPLO":"Ejemplo",
-			"MNU_EJEMPLO2":"Ejemplo 2"
+			"MNU_EJEMPLO":"Menu 2",
+			"MNU_EJEMPLO2":"Ejemplo 2",
+			"MNU_EJEMPLO5":"Menu 5",
+			"MNU_EJEMPLO6":"Menu 6",
+			
         }
     }
 }

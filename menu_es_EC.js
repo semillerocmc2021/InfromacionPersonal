fef8aa4acd0cca2161ed01c1c53a90dceda52d98
Yina Reyes <yinareyes02@gmail.com>
2021-03-22 16:11:58 -0500

    se añade menu

--- menu_es_EC.js
+++ menu_es_EC.js
@@ -5,13 +5,13 @@
 			"MNU_LUIS_ANGEL_FLOREZ":"Menú Luis Florez",
 			"MNU_MFFR":"Maria Fernanda Florez",
 			"MNU_PLCO_ALEXANDER":"Menu Alexander Pilco",
-			"MNU_YINA_REYES":"Menu Yina Reyes Polo",
 			"MNU_JEAN_RAVE":"Jean Rave",
 			"MNU_JCRR_GRID":"registrar Producto Jean Rave",
             "MNU_JCRR_GRID2":"Lista Productos Jean Rave",
             "MNU_JCRR_GRID_CLIENTE":"Busqueda Clientes Jean Rave",
 			"MNU_DGRA":"Menu Dorian Ramos",
-            "MNU_JMS":"Menu Jorge Mosquera"
+            "MNU_JMS":"Menu Jorge Mosquera",
+			"MNU_YINA_REYES":"Menu Yina Reyes"
 
         }
 	}

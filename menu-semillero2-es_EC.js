56dead58cb606676268d067e8b277ad1b5683365
jorge <jorgemosquera1150@gmail.com>
2021-03-22 17:08:44 -0500

    Etiqueta ingresar producto

--- menu-semillero2-es_EC.js
+++ menu-semillero2-es_EC.js
@@ -27,7 +27,8 @@
 			"MNU_ROGER_REYES":"Menu ROGER",
 			"MNU_FASE_4":"Fase 4",
 			"MNU_JMS_BCLI":"Busqueda clientes Jorge Mosquera",
-			"MNU_JMS_VPRO":"Productos Jorge Mosquera"
+			"MNU_JMS_VPRO":"Productos Jorge Mosquera",
+			"MNU_JMS_PRO":"Ingresar pro Jorge Mosquera"
 
 
         }

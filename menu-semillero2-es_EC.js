0fe794c325d2d429debaebcb1b7ff131b92ca112
bapilco <bryanpilco.1597@gmail.com>
2021-03-22 17:09:33 -0500

    menu Alexander modificado

--- menu-semillero2-es_EC.js
+++ menu-semillero2-es_EC.js
@@ -4,7 +4,7 @@
             "MNU_FASE_4":"Men\u00FA Fase 4",
 			"MNU_LUIS_ANGEL_FLOREZ":"Menú Luis Florez",
 			"MNU_MFFR":"Maria Fernanda Florez",
-			"MNU_PLCO_ALEXANDER":"Menu Alexander Pilco",
+			"MNU_PLCO_ALEXANDER":"Alexander Pilco",
 			"MNU_JEAN_RAVE":"Jean Rave",
 			"MNU_JCRR_GRID":"registrar Producto Jean Rave",
             "MNU_JCRR_GRID2":"Lista Productos Jean Rave",

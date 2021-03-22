b4ccc5bc18f04493ed8a1ff805aabed30a1dbb71
Yina Reyes <yinareyes02@gmail.com>
2021-03-22 15:51:54 -0500

    Se añade menu

--- menu_es_EC.js
+++ menu_es_EC.js
@@ -4,7 +4,8 @@
             "MNU_FASE_4":"Menú Fase 4",
 			"MNU_LUIS_ANGEL_FLOREZ":"Menú Luis Florez",
 			"MNU_MFFR":"Maria Fernanda Florez",
-			"MNU_PLCO_ALEXANDER":"Menu Alexander Pilco"
+			"MNU_PLCO_ALEXANDER":"Menu Alexander Pilco",
+			"MNU_YINA_REYES":"Menu Yina Reyes"
         }
     }
 }

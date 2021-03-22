9d27ec76ff081e35bc349d909a527b58ff92a7d3
luisangel1808 <luisangel1808@hotmail.com>
2021-03-22 17:30:55 -0500

    Cambio de nombre

--- menu-semillero2-es_EC.js
+++ menu-semillero2-es_EC.js
@@ -2,7 +2,7 @@
     "COMMONS": {
         "MENU": {
             "MNU_FASE_4":"Men\u00FA Fase 4",
-			"MNU_LUIS_ANGEL_FLOREZ":"Capacitaci&oacute;n Luis Fl&oacute;rez",
+			"MNU_LUIS_ANGEL_FLOREZ":"Capacitaci\u00F3n Luis Fl\u00F3rez",
 			"MNU_MFFR":"Capacitaci\u00F3n Mar\u00EDa Fl\u00F3rez",
 			"MNU_PLCO_ALEXANDER":"Menu Alexander Pilco",
 			"MNU_JEAN_RAVE":"Capacitaci\u00F3n Jean Rave",
